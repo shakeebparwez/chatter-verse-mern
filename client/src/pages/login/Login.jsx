75158b40a23b2b186b7469648a5c6118b629f068
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-07-28 01:40:39 +0530

    Register and login ui

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const login = async () => {
+    console.log(user);
+  }
+
   return (
-    <div>Login</div>
+    <div className="h-screen bg-primary flex items-center justify-center">
+      <div className="bg-white shadow-md p-5 flex flex-col gap-5 w-96">
+        <h1 className="text-2xl uppercase font-semibold text-primary">ChatterVerse Login</h1>
+        <hr />
+        <input type="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="Enter your email" />
+        <input type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="Enter your password" />
+
+        <button className="contained-btn" onClick={login}>Login</button>
+
+        <Link to="/register" className="underline">Don't have an account? Register</Link>
+      </div>
+    </div>
   )
 }
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export const Register = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const register = async () => {
+    console.log(user);
+  }
+
   return (
-    <div>Register</div>
+    <div className="h-screen bg-primary flex items-center justify-center">
+      <div className="bg-white shadow-md p-5 flex flex-col gap-5 w-96">
+        <h1 className="text-2xl uppercase font-semibold text-primary">ChatterVerse Register</h1>
+        <hr />
+        <input type="text" value={user.name} onChange={(e) => setUser({ ...user, name: e.target.value })} placeholder="Enter your name" />
+        <input type="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="Enter your email" />
+        <input type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="Enter your password" />
+
+        <button className="contained-btn" onClick={register}>Register</button>
+
+        <Link to="/login" className="underline">Already have an account? Login</Link>
+      </div>
+    </div>
   )
 }
